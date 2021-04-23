b257e05813cf80690d691cef05d229841e600498
Luism27 <54760238+Luism27@users.noreply.github.com>
2021-04-23 15:24:32 -0500

    keys values

--- front/src/config/fire.js
+++ front/src/config/fire.js
@@ -2,6 +2,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
+    apiKey: "AIzaSyCSdd6XvqQX6Wq5vOUaBhqu2najgJz_39c",
+    authDomain: "neurons-f0ef3.firebaseapp.com",
+    projectId: "neurons-f0ef3",
+    storageBucket: "neurons-f0ef3.appspot.com",
+    messagingSenderId: "689189209717",
+    appId: "1:689189209717:web:76b5346dc618ad8e5a9a3c",
+    measurementId: "G-BDKZCRXY6X"
 }
 
 try {
